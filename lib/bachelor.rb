c4521ee3af825593d96bd906b1f9ccf6b8253938
Victoria B <viktorija.bourassa@gmail.com>
2018-12-26 20:50:12 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -52,6 +52,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |elem|
     age = elem["age"].to_i
     ages << age
-      return ages
+    average = ages.inject{ |sum, el| sum + el }.to_f / ages.size
+    return average.to_i
     end
     end
